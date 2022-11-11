d2a22f9cf9a40cba93875d9dcea23179d1b0d94e
Ekaterina Kalache <ekaterinakalache@Ekaterinas-MacBook-Air.local>
2022-11-10 19:06:01 -0500

    initial

---
+++ website/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Patents
+
+def get_data(request):
+    if request.method == 'POST':
+        searched = request.POST['searched']
+        print(searched)
+        patents = Patents.objects.raw("SELECT id, patent_id FROM patents WHERE MATCH(patent_text) AGAINST ('%s' IN NATURAL LANGUAGE MODE)" %searched)
+        return render(request, 'search-patents.html', {'patents': patents})
+    else:
+        return render(request, 'search-patents.html')
